0bd872ecebcf8aa48e1d39d2f43318a034cd00a8
Miguel Favio <miguelfavioqm@gmail.com>
2023-10-15 19:48:17 -0400

    Add Products Container API movie db

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,24 +1,72 @@
 import React from "react";
 import Product from "./Product";
-
-const ProductsContainer = () => {
+import { useEffect, useState } from "react";
+const ProductsContainer = ({}) => {
+  const [data, setData] = useState({});
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [title2, setTitle2] = useState("");
+  const [desc2, setDesc2] = useState("");
+  const [img1, setImg1] = useState("");
+  const [img2, setImg2] = useState("");
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      alert(error);
+    }
+  }, []);
+  const urlImages = "https://image.tmdb.org/t/p/w600_and_h900_bestv2";
+  const url =
+    "https://api.themoviedb.org/3/discover/movie?language=es-US&page=1";
+  const dataAPI = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjliNWY4YWRiMDMyMDFhNjk3MTY3YmVhMGRkYTMwYiIsInN1YiI6IjY1MmMyYTkyMDI0ZWM4MDBhZWNjMDViNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kt9r6J_rTAGTG7CJ-DyiwDdLM8ZY3tksh-Tz5APkrl8",
+    },
+  };
+  const fetchData = async () => {
+    const dataf = await fetch(url, dataAPI);
+    const dataJSON = await dataf.json();
+    setData(dataJSON);
+    setTitle(dataJSON.results[0].title);
+    setDesc(dataJSON.results[0].overview);
+    setTitle2(dataJSON.results[1].title);
+    setDesc2(dataJSON.results[1].overview);
+    setImg1(dataJSON.results[0].poster_path);
+    setImg2(dataJSON.results[1].poster_path);
+  };
   return (
     <section className="overflow-hidden container-md mt-2 flex gap-4 flex-col-reverse md:flex-row items-center">
-      <div className="rounded-lg bg-red-400/50  items-center p-5 flex-col w-80 h-80 justify-between hidden md:flex">
+      <div className="rounded-lg  items-center flex-col w-80 h-80 justify-between hidden md:flex">
+        <img
+          src={urlImages + img1}
+          alt="IMG1"
+          width={200}
+          className="rounded-lg"
+        />
         <h4 className="font-black">ESTRENO</h4>
-        <div className="bg-red-400/70 p-2 rounded-lg">
-          <h3 className="font-bold text-center">TITULO</h3>
-          <p className="text-center px-2 mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <div className="bg-red-500/70 p-2 rounded-lg absolute bottom-1/4 text-white flex flex-col items-center justify-center">
+          <h3 className="font-bold text-center text-xs">{title}</h3>
+          <p className="text-center px-2 mt-2 text-xs line-clamp-5 max-w-[10rem]">
+            {desc2}
           </p>
         </div>
       </div>
-      <div className="rounded-lg bg-red-400/50 flex items-center p-5 flex-col w-80 h-80 justify-between mb-4 md:!mb-0">
+      <div className="rounded-lg flex items-center flex-col w-80 h-80 justify-between mb-4 md:!mb-0">
+        <img
+          src={urlImages + img2}
+          alt="IMG1"
+          width={200}
+          className="rounded-lg"
+        />
         <h4 className="font-black">ESTRENO</h4>
-        <div className="bg-red-400/70 p-2 rounded-lg">
-          <h3 className="font-bold text-center">TITULO</h3>
-          <p className="text-center px-2 mt-2">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <div className="bg-red-500/70 p-2 rounded-lg absolute md:bottom-1/4 text-white flex flex-col items-center justify-center">
+          <h3 className="font-bold text-center text-xs">{title2}</h3>
+          <p className="text-center px-2 mt-2 text-xs line-clamp-5 max-w-[10rem]">
+            {desc2}
           </p>
         </div>
       </div>
