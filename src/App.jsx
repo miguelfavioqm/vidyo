5315d2a85c60690c2a7de0721a88fb521381b5f7
Miguel Favio <miguelfavioqm@gmail.com>
2023-10-16 16:00:31 -0400

    fix number

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <a
-        href="https://api.whatsapp.com/send?phone=59164121283&text=HOLA%20QUIERO%20MAS%20INFORMACION%20DE%20LAS%20PLATAFORMAS"
+        href="https://api.whatsapp.com/send?phone=59176281196&text=HOLA%20QUIERO%20MAS%20INFORMACION%20DE%20LAS%20PLATAFORMAS"
         className="fixed bottom-2 right-2 md:text-5xl text-green-600 border-2 border-green-600 rounded-full md:p-2 hover:animate-bounce  text-xl p-1 z-20"
         target="_blank"
       >
